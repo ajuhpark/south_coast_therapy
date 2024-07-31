eee707e5c23665528b80d42c4e0d013f560c6db0
ajuhpark <ajuhpark@gmail.com>
2024-07-30 23:10:16 -0700

    adding to Camille Mormal Tutorial, still have stuff to figure out.

---
+++ 10.vite.config.js
@@ -0,0 +1,39 @@
+// 10.vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+
+  build: {
+    server: {
+      host: 'localhost',
+      cors: '*',
+      hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+      },
+    },
+    // this building a library is from this website: https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
+    /*
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'webflow_js_library',
+      fileName: (format) => `webflow_js_library.${format}.js`
+    },
+    */
+    // I put this back from the original webflow + vite.js file.
+    rollupOptions: {
+      input: 'lib/example_8.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'example_8.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  }
+});
