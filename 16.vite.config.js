722b1ffc0affb8548d5a583e7081c8ead00edd87
Andrew <ajuhpark@gmail.com>
2025-01-29 16:39:39 -0800

    trying out example_12.js

---
+++ 16.vite.config.js
@@ -0,0 +1,39 @@
+// 16.vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    server: {
+      host: 'localhost',
+      cors: '*',
+      hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+      },
+    },
+    // this building a library is from this website: https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
+    /*
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'webflow_js_library',
+      fileName: (format) => `webflow_js_library.${format}.js`
+    },
+    */
+    // I put this back from the original webflow + vite.js file.
+    rollupOptions: {
+      input: 'lib/example_12.js',
+      output: {
+        dir: 'dist/example_12', // Output directory unique to this build
+        format: 'es',
+        entryFileNames: '[name].js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  },
+})
