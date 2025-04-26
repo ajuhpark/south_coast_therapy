46d35cac12c61986a5340c531d1727803713313f
Andrew <ajuhpark@gmail.com>
2025-04-26 09:38:37 -0700

    putting in some code for the card stack  module.

---
+++ 23.vite.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  server: {
+    cors: true, // This is simpler and allows any origin during development
+    host: true,
+    port: 5173, // Explicitly set the port
+    strictPort: true, // Don't try alternative ports
+    hmr: {
+      clientPort: 5173 // Ensure client connects to same port
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: 'lib/example_16.js',
+      output: {
+        dir: 'dist/example_16',
+        format: 'es',
+        entryFileNames: '[name].js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  },
+})
